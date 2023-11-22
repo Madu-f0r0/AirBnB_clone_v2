56a6484d1be93e46bdcd76d6a4f87a1d143d3be1
Jukunye <jukunyes@gmail.com>
2023-11-22 21:32:41 +0300

    reverted

--- models/place.py
+++ models/place.py
@@ -23,15 +23,8 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=False)
     amenity_ids = []
 
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id' ,String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                          Column('amenity_id' ,String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
-
     if env == "db":
         reviews = relationship(
                 "Review", backref="place", cascade="all, delete")
-        amenities = relationship(
-                "Amenity", secondary='place_amenity', backref="places", viewonly=False)
     else:
         pass
-    
