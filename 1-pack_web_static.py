d0f9be8207a7aee78e3128a919fafd837183b790
Madu-f0r0 <duherbert@gmail.com>
2023-12-08 07:30:28 -0800

    Script 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,18 +2,14 @@
 """ This script generates a `.tgz` archive """
 
 from datetime import datetime
-from os import mkdir
+from fabric.api import local
 
 
 def do_pack():
     """Creates a `.tgz` archive from every file in `webstatic` directory"""
     now = datetime.now().strftime("%Y%m%d%H%M%S")
-    try:
-        mkdir("versions")
-    except FileExistsError:
-        pass
-    except Exception:
-        print("Directory could not be created")
+
+    local("mkdir -p versions")
 
     archive_path = "versions/web_static_{}.tgz".format(now)
     result = local("tar -cvzf {} web_static".format(archive_path))
