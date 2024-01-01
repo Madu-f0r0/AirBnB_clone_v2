5704ae6b58ffad110adca6cdb2868eb469071569
Madu-f0r0 <duherbert@gmail.com>
2024-01-01 16:18:51 +0100

    Fixed slight changes to files for task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,20 +6,22 @@ Listening port: 5000
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
+
 
 @app.teardown_appcontext
 def remove_session(exc):
     """Removes the current SQLAlchemy session"""
     storage.close()
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays dynamic HTML page for URI `/states_list`"""
     states = storage.all("State").values()
     return render_template("7-states_list.html", states=states)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
